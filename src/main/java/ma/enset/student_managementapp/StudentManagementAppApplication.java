52fb54d7074087ff3c707f769225dd92e5578f1a
Abdenasser Aroukhsiss <a.aroukhsiss_etu@enset-ac.ma>
2022-04-24 16:13:21 +0000

    Initial commit

---
+++ src/main/java/ma/enset/student_managementapp/StudentManagementAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.student_managementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/student_managementapp/StudentManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.student_managementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
